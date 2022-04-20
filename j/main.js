37f8739035038e72ecc5fdbf9e61c543871d27ad
Subha <subhaban20@gmail.com>
2022-04-19 17:12:46 -0700

    initial commit for lab6-fetch

---
+++ j/main.js
@@ -0,0 +1,39 @@
+
+//This code does NOT create any global variables.
+//Promises can be chained together, with the previous promise
+// passing its results to the next one in the chain.
+// the format is: fetch().then().then().catch()
+//it's easier to read if we put each step in its own line,
+//that's why the periods start the then lines.
+
+fetch("houses.json")
+    .then((response) => response.json())
+    .then((data) => {
+        //create a temp holder to append all the html generated inside the forEach iterator
+        let html = "";
+       // html +=`<dl class="datalist">`;
+        //the argument "house" passed to the arrow function
+        //holds each item in the array in turn.
+        data.forEach((house) => {
+            let family = house.members.join(" | ");
+            
+            // generate the html snippet for one array item
+            //to be added to the "html" temp holder.
+           // let dt =`<dt class="housename">${house.name}</dt>`;
+           // html += dt;
+           // let dd=`<dd class="membername">${family}</dd>`;
+           // html += dd;
+            
+            let objInfo = `<p class="house">${house.name}</p>
+            <p class="folks">${family}</p>`;
+             html += objInfo;
+            
+        });
+        //html += `</dl>`;  
+        //make a reference to the html container where
+        //the info will be displayed.
+        const container = document.querySelector("#container");
+        container.innerHTML = html;
+    })
+    .catch((err) => console.log("Oops!", err));
+    //this only runs if there is an error during the above process
